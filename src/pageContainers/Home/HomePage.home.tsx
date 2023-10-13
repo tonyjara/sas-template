95587e53395a9f881d213749f95d9181dcd443f0
Tony Jara <340studiospy@gmail.com>
2023-10-12 23:48:40 -0300

    ci test

--- src/pageContainers/Home/HomePage.home.tsx
+++ src/pageContainers/Home/HomePage.home.tsx
@@ -77,8 +77,8 @@ export default function HomePage() {
   return (
     <Box px={{ base: 3, md: 5 }} py={{ base: 3, md: 3 }}>
       <DynamicTable
-        rowActions={handleRowClick}
         /* loading={isLoading} */
+        rowActions={handleRowClick}
         enableColumnFilters={true}
         /* whereFilterList={whereFilterList} */
         /* setWhereFilterList={setWhereFilterList} */
