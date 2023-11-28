c4b2ace3f6b300a8e532dc608b379e05ac09f5d8
Tony Jara <340studiospy@gmail.com>
2023-11-27 21:55:52 -0300

    change working for create user modal

--- src/components/Modals/CreateUser.modal.tsx
+++ src/components/Modals/CreateUser.modal.tsx
@@ -12,10 +12,9 @@ import {
   Container,
 } from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import type { Account, Role } from "@prisma/client";
+import type { Role } from "@prisma/client";
 import React from "react";
 import { useForm } from "react-hook-form";
-import { knownErrors } from "../../lib/dictionaries/knownErrors";
 import { trpcClient } from "@/utils/api";
 import {
   FormCreateUser,
@@ -36,7 +35,6 @@ const CreateUserModal = ({
   onSubmit?: any;
 }) => {
   const trpcContext = trpcClient.useUtils();
-  const { onCopy, value, setValue, hasCopied } = useClipboard("");
 
   const {
     handleSubmit,
@@ -48,7 +46,6 @@ const CreateUserModal = ({
     resolver: zodResolver(validateUserCreate),
   });
   const handleOnClose = () => {
-    setValue("");
     reset(defaultUserCreateValues);
     onClose();
   };
@@ -77,52 +74,38 @@ const CreateUserModal = ({
       <form onSubmit={handleSubmit(onSubmit ?? submitFunc)} noValidate>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Create User</ModalHeader>
+          <ModalHeader>Invite Team Member</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            {value.length > 0 && (
-              <Container textAlign={"center"}>
-                <Text fontWeight={"bold"} fontSize={"xl"}>
-                  Share this link with the user
-                </Text>
-                <Button onClick={onCopy} mb={10} mt={1}>
-                  {hasCopied ? "Copied!" : "Copy invite"}
-                </Button>
-              </Container>
-            )}
-            {!value.length && (
-              <>
-                <FormControlledText
-                  control={control}
-                  errors={errors}
-                  name="name"
-                  label="User name"
-                  autoFocus={true}
-                />
-                <FormControlledText
-                  control={control}
-                  errors={errors}
-                  name="email"
-                  label="Email"
-                  helperText="Please enter a valid email address"
-                />
-                <FormControlledSelect
-                  control={control}
-                  errors={errors}
-                  name="role"
-                  label="Select a role"
-                  options={roleOptions ?? []}
-                />
-              </>
-            )}
+            <Text color={"gray.500"} mb="20px">
+              After submitting the form, the user will receive a link to assign
+              a password to their account via email.
+            </Text>
+            <FormControlledText
+              control={control}
+              errors={errors}
+              name="name"
+              label="User name"
+              autoFocus={true}
+            />
+            <FormControlledText
+              control={control}
+              errors={errors}
+              name="email"
+              label="Email"
+              helperText="Please enter a valid email address"
+            />
+            <FormControlledSelect
+              control={control}
+              errors={errors}
+              name="role"
+              label="Select a role"
+              options={roleOptions ?? []}
+            />
           </ModalBody>
 
           <ModalFooter>
-            <Button
-              isDisabled={isLoading || isSubmitting || !!value}
-              type="submit"
-              mr={3}
-            >
+            <Button isDisabled={isLoading || isSubmitting} type="submit" mr={3}>
               Save
             </Button>
             <Button colorScheme="gray" mr={3} onClick={handleOnClose}>
